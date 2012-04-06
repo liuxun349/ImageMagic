df8dcc344ba6708d6216282e436c13081ea78b1f
Jay_qiu <840845087@qq.com>
2012-04-06 08:33:47 +0800

    tian jia zhao xiang gong neng

--- src/com/ImageMagic/view/ImageHandleActivity.java
+++ src/com/ImageMagic/view/ImageHandleActivity.java
@@ -1,21 +1,113 @@
 package com.ImageMagic.view;
 
+import java.io.FileNotFoundException;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
+import android.view.Display;
+import android.widget.ImageView;
 
 public class ImageHandleActivity extends Activity {
 	private Handler mHandler;
+	private ImageView ImageV;
+	Bitmap mBitmap;
+	public static final String IMAGE_UNSPECIFIED = "image/*";
+	public static final int IMAGECUT = 1;
+	public Uri uri;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.handle_main);
-		
-		mHandler = new Handler(){
+
+		ImageV = (ImageView) findViewById(R.id.imageview);
+		Intent intent = getIntent();
+		String path = intent.getStringExtra("path");
+		Display display = this.getWindowManager().getDefaultDisplay();
+
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		mBitmap = BitmapFactory.decodeFile(path, options);
+		uri = Uri.parse(MediaStore.Images.Media.insertImage(
+				getContentResolver(), mBitmap, null, null));
+		imageCut(uri);
+		System.out.println(mBitmap);
+		ImageV.setImageBitmap(mBitmap);
+
+		mHandler = new Handler() {
 			public void handleMessage(Message message) {
-				
+
 			}
 		};
+	}
+
+	public void imageCut(Uri uri) {
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setDataAndType(uri, IMAGE_UNSPECIFIED);
+		intent.putExtra("crop", "true");
+		// aspectX aspectY 是宽高的比例
+		intent.putExtra("aspectX", 1);
+		intent.putExtra("aspectY", 1.5);
+		// outputX outputY 是裁剪图片宽高
+		intent.putExtra("outputX", 450);
+		intent.putExtra("outputY", 600);
+		intent.putExtra("return-data", true);
+		startActivityForResult(intent, IMAGECUT);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK && data != null) {
+			switch (requestCode) {
+			case IMAGECUT:
+
+				try {
+					Uri uri = null;
+					Bitmap map1 = data.getParcelableExtra("data");
+					uri = Uri.parse(MediaStore.Images.Media.insertImage(
+							getContentResolver(), map1, null, null));
+					int dw = this.getWindowManager().getDefaultDisplay()
+							.getWidth();
+					int dh = this.getWindowManager().getDefaultDisplay()
+							.getHeight();
+					BitmapFactory.Options op = new BitmapFactory.Options();
+					op.inJustDecodeBounds = true;
+					Bitmap pic;
+					pic = BitmapFactory.decodeStream(this.getContentResolver()
+							.openInputStream(uri), null, op);
+					int wRatio = (int) Math.ceil(op.outWidth / (float) dw); // 计算宽度比例
+					int hRatio = (int) Math.ceil(op.outHeight / (float) dh); //
+					// 计算高度比例
+
+					if (wRatio > 1 && hRatio > 1) {
+						if (wRatio > hRatio) {
+							op.inSampleSize = wRatio;
+						} else {
+							op.inSampleSize = hRatio;
+						}
+					}
+					op.inJustDecodeBounds = false;
+					pic = BitmapFactory.decodeStream(this.getContentResolver()
+							.openInputStream(uri), null, op);
+
+					ImageV.setImageBitmap(map1);
+				} catch (FileNotFoundException e) {
+
+					e.printStackTrace();
+				}
+
+				break;
+			default:
+				break;
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 }
--- src/com/ImageMagic/view/ImageMagicMainActivity.java
+++ src/com/ImageMagic/view/ImageMagicMainActivity.java
@@ -1,14 +1,13 @@
 package com.ImageMagic.view;
 
-import org.w3c.dom.Text;
-
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.provider.MediaStore.Audio.Media;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -20,6 +19,8 @@ public class ImageMagicMainActivity extends Activity {
 	/** Called when the activity is first created. */
 	private Button takePhoto, localAlbum, networkAlubum;
 	private static final int FLAG_CHOOSE = 1;
+	private static final int PHOTOGRAPH = 2;
+	public static final String IMAGE_UNSPECIFIED = "image/*";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -42,13 +43,18 @@ public class ImageMagicMainActivity extends Activity {
 			// TODO Auto-generated method stub
 			switch (v.getId()) {
 			case R.id.btn1:
-
+				Intent intent_potograph = new Intent(
+						MediaStore.ACTION_IMAGE_CAPTURE);
+				ContentValues values = new ContentValues(); 
+				values.put(MediaStore.Images.Media.DISPLAY_NAME, "testing"); 
+				startActivityForResult(intent_potograph, PHOTOGRAPH);
 				break;
 
 			case R.id.btn2:
 				Intent intent = new Intent();
-				intent.setAction(intent.ACTION_PICK);
-				intent.setType("image/*");
+				intent.setAction(intent.ACTION_GET_CONTENT);
+				intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+						IMAGE_UNSPECIFIED);
 				startActivityForResult(intent, FLAG_CHOOSE);
 				break;
 			case R.id.btn3:
@@ -59,12 +65,18 @@ public class ImageMagicMainActivity extends Activity {
 	};
 
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		System.out.println("requestCode:" + requestCode + "resultCode:"
+				+ resultCode + "data:" + data);
+		super.onActivityResult(requestCode, resultCode, data);
 		if (resultCode == RESULT_OK && data != null) {
 			switch (requestCode) {
+
 			case FLAG_CHOOSE:
 				Uri uri = data.getData();
 				Log.d("LLH===>",
-						"uri=" + uri + ",authority=" + uri.getAuthority()+" "+uri.getPath());
+						"uri=" + uri + ",authority=" + uri.getAuthority() + " "
+								+ uri.getPath());
 				if (!TextUtils.isEmpty(uri.getAuthority())) {
 					Cursor cursor = getContentResolver().query(uri,
 							new String[] { MediaStore.Images.Media.DATA },
@@ -78,18 +90,43 @@ public class ImageMagicMainActivity extends Activity {
 					cursor.moveToFirst();
 					String path = cursor.getString(cursor
 							.getColumnIndex(MediaStore.Images.Media.DATA));
-					Log.d("LLH===>", path+"  ");
-					Intent intent = new Intent(this,ImageHandleActivity.class);
+					Log.d("LLH===>", path + "  ");
+					Intent intent = new Intent(this, ImageHandleActivity.class);
 					intent.putExtra("path", path);
+					Toast.makeText(this, path, Toast.LENGTH_LONG).show();
 					startActivity(intent);
-				}else{
-					Intent intent = new Intent(this,ImageHandleActivity.class);
+				} else {
+					Intent intent = new Intent(this, ImageHandleActivity.class);
 					intent.putExtra("path", uri.getPath());
+					Toast.makeText(this, uri.getPath(), Toast.LENGTH_LONG)
+							.show();
 					startActivity(intent);
 				}
 				break;
+
+			case PHOTOGRAPH:
+				
+				Bitmap photo = data.getParcelableExtra("data");
+				Uri uriP = Uri.parse(MediaStore.Images.Media.insertImage(
+						getContentResolver(), photo, null, null));
+				
+				String[] projection = { MediaStore.Images.Media.DATA };
+				Cursor ActualImageCursor = managedQuery(uriP, projection, null,
+						null, null);
+				
+				int column_index = ActualImageCursor
+						.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+				
+				ActualImageCursor.moveToFirst();
+				String path = ActualImageCursor.getString(column_index);
+				
+				Intent intent = new Intent(ImageMagicMainActivity.this,
+						ImageHandleActivity.class);
+				
+				intent.putExtra("path", path);
+				startActivity(intent);
+				break;
 			}
 		}
-	};
-
+	}
 }
